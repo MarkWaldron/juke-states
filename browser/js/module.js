73bedc9544dcf2962bb51e5fbaebcbb55b121ce3
MarkWaldron <markewaldron@gmail.com>
2015-12-04 16:08:13 -0500

    All albums and artists loading

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,1 +1,17 @@
-var app = angular.module('juke', []);
+var app = angular.module('juke', ['ui.router']);
+
+app.config(function ($stateProvider){
+  $stateProvider.state('albumList', {
+    url: '/albums',
+    controller: 'AlbumsCtrl',
+    templateUrl: '/templates/albums.html'
+  });
+});
+
+app.config(function ($stateProvider){
+  $stateProvider.state('artistList', {
+    url: '/artists',
+    controller: 'ArtistsCtrl',
+    templateUrl: '/templates/artists.html'
+  });
+});
